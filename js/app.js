124cc1803400f9461e1473b81aac7def2765844b
mel1bt <mberrocal.tipte@gmail.com>
2016-10-18 16:34:52 -0500

    implementando google Maps

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   $("#sigValidacion").click(function(evento){
     location.href="sign_up.html";
   });
+  
   $("#celular").keydown(function(evento) {
 		var ascii = evento.keyCode;
 		if (ascii == 8 || (ascii >= 48 && ascii <= 57)) {
@@ -11,6 +12,7 @@ $(document).ready(function(){
 			return false;
 		}
 	});
+  
   $("#celular").keyup(function(evento) {
     var longitud = $(this).val().length;
 		if (longitud == 9) {
@@ -20,17 +22,33 @@ $(document).ready(function(){
 		}
 	});
    
-  
   $("#valCelular").click(function(evento){
-    
-    
     var numeroAleatorio = Math.random();
     var codigoVal = Math.floor(numeroAleatorio*89+99);
     var numValido = localStorage.setItem("numValidacion", codigoVal);
     alert("LAB -  "+localStorage.getItem("numValidacion"));
     location.href="validacion.html";
   });
+  
+  $(".numValido").keydown(function(evento) {
+		var ascii = evento.keyCode;
+		if (ascii == 8 || (ascii >= 48 && ascii <= 57)) {
+			return true;
+		} else {
+			return false;
+		}
+	});
     
+  $("#numVal1").keyup(function(e){
+  $("#numVal2").focus();                  
+  });
+  $("#numVal2").keyup(function(e){
+  $("#numVal3").focus();                  
+  });
+  
+  
+  
+  
   $("#btnValido").click(function(evento){
     var numVal = localStorage.getItem("numValidacion");
     console.log(numVal);
@@ -43,20 +61,25 @@ $(document).ready(function(){
   });
   
   $("#btnRegistro").click(function(evento){
-    var nombre = localStorage.setItem("nameReg", $("#nameVal").val);
-    var apellido = localStorage.setItem("apeReg", $("#lasVal").val);
-    var email = localStorage.setItem("mailReg", $("mailVal").val);
-    alert("bien"+ localStorage.getItem("nameReg"));
-    //location.href="home_map.html";
+    localStorage.setItem("nameReg", $("#nameVal").val());
+    localStorage.setItem("apeReg", $("#lasVal").val());
+    localStorage.setItem("mailReg", $("mailVal").val());
+    
+    location.href="home_map.html";
+   
   });
-  /*
-  $("#map").googleMap({
-    zoom: 10,
-    coords: [48.895651, 2.290569], 
-    type: "ROADMAP"
-  });*/
   
   
+    function validarNum(e){
+     
+		var ascii = evento.keyCode;
+		if (ascii == 8 || (ascii >= 48 && ascii <= 57)) {
+			return true;
+		} else {
+			return false;
+
+	};
+  };
   
    
 });
